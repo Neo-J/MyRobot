a2f723d26e0d9c24eff2b2a9d65e98966229d25e
NeoJ <neoj@neoj-Aspire-4741.(none)>
2013-08-27 21:05:34 +0800

    Bot类用于初始化box

--- Bot.rb
+++ Bot.rb
@@ -1,1 +1,35 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__)) 
+
+require 'yaml'
+require 'wordplay'
+
+class Bot
+	attr_reader :name
+
+	def initialize(options)
+		@name = options [:name] || "Unnamed Bot"
+		begin
+			@data = YAML.load(File.read(options[:data_file]))
+		rescue
+			raise "Can't load bot data"
+		end
+	end
+
+	#greeting function
+	def greeting
+		random_index(:greeting)
+	end
+
+	#farewell function
+	def farewell
+		random_index(:farewell)
+	end
+
+	private
+
+	#a random function
+	def random_response(key)
+		random_index = rand(@data[:responses][key].length)
+		@data[:responses][key][random_index].gsub(/\[name\]/,@name)
+	end
+end
